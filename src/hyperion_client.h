3972010a3f4aeb828663ddcef5c352acb447ed68
Pavel Turbin <p.turbin@berizaryad.ru>
2024-08-03 09:57:54 +0100

    added nv12 passthrough

--- src/hyperion_client.h
+++ src/hyperion_client.h
@@ -7,3 +7,7 @@ int hyperion_read();
 int hyperion_destroy();
 int hyperion_set_image(const unsigned char* image, int width, int height);
 int hyperion_set_register(const char* origin, int priority);
+
+// Debug
+int debug_client(const char* hostname, int port);
+int hyperion_set_nv12_image(const unsigned char* y_data, const unsigned char* uv_data, int width, int height, int stride);
